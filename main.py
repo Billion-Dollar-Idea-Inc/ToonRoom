1b079110cec9c988c091867af7e7afde24976e75
harrisongerst <hjgerstenlauer@gmail.com>
2015-11-14 19:04:42 -0500

    taylor fixed it

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from flask import Flask
-from flask import render_template
+from flask import *
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def TuneRoomHome():
+def index():
     return render_template('index.html')
 
-@app.route('/chat')
-def TuneRoomChat():
-		return render_template('chat.html')
+@app.route('/join', methods=['POST'])
+def join():
+	room_num = random.randint(1, 1000000)
+	# Redirect to /chat/<room>
+	# DB find
+	return redirect(url_for('chat', room=room_num))
+
+@app.route('/chat/<room>', methods =['GET', 'POST'])
+def chat(room):
+	# Room validation code
+	return render_template('chat.html', room_num=room)
 
 if __name__ == '__main__':
     app.debug = True
