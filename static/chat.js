791adadd89c6c038da48b8ae4a83821635ac19cf
Aiden Cullo <culloaiden3@gmail.com>
2015-11-15 00:54:58 -0500

    added functionality in chat screen

--- static/chat.js
+++ static/chat.js
@@ -1,25 +1,38 @@
 var link;
 function revealBox(){
-	document.getElementById("plus_sign").style.visibility = "hidden";
-	document.getElementById("input").style.visibility = "visible";
-	document.getElementById("submit").style.visibility = "visible";
+	if(document.getElementById('plus_sign').value != "Submit"){
+
+		document.getElementById('plus_sign').value = "Submit";
+		document.getElementById('input').style.visibility = "visible";
+	}
+	else{
+		if(document.getElementById('plus_sign').value == "Submit"){
+		document.getElementById('plus_sign').value = "+";
+		back_to_plus();
+		}
+	}	
 }
 function back_to_plus(){
 	link = document.getElementById('input').value;
-	//alert(link);
-	document.getElementById("plus_sign").style.visibility = "visible";
-	document.getElementById("input").style.visibility = "hidden";
-	document.getElementById("submit").style.visibility = "hidden";
+	alert(link);
 	update_video()
+	document.getElementById('input').value = "";
+	document.getElementById('plus_sign').style.visibility = "visible";
+	document.getElementById('input').style.visibility = "hidden";
+	alert("reached");
 }
 function update_video(){
+	alert("in vid");
 	var id = link.substring(32);
+	alert("1/2");
 	var iframe = document.createElement("iframe");
+	alert("1");
     iframe.setAttribute("src",
           "https://www.youtube.com/embed/" + id 
         + "?rel=0&amp;autoplay=1&amp;showin­fo=0&amp;controls=0&amp;HD=1;iv_load_policy=3");
-    
+    alert("2");
     iframe.style.width  = 400;
     iframe.style.height = 300;
-	document.body.appendChild(iframe)
+	document.body.appendChild(iframe);
+	alert("at end");
 }
