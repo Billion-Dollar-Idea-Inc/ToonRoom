8eb529cc1b0d5f648cb3bf09db996d09b67da208
Ethan Schoen <e.schoen@yahoo.com>
2015-11-15 02:29:52 -0500

    stuff

--- static/chat.js
+++ static/chat.js
@@ -1,5 +1,3 @@
-var link;
-var iframe;
 function revealBox(){
 	if(document.getElementById('plus_sign').value != "Submit"){
 
@@ -14,21 +12,57 @@ function revealBox(){
 	}	
 }
 function back_to_plus(){
-	link = document.getElementById('input').value;
-	alert(link);
-	update_video()
+	var link = document.getElementById('input').value;
+	
+	change_video('pvNPORFXpc');
+
 	document.getElementById('input').value = "";
 	document.getElementById('plus_sign').style.visibility = "visible";
 	document.getElementById('input').style.visibility = "hidden";
-	alert("reached");
 }
-function update_video(){
-	var id = link.substring(32);
-	iframe = document.createElement("iframe");
-    iframe.setAttribute("src",
-          "https://www.youtube.com/embed/" + id 
-        + "?rel=0&amp;autoplay=1&amp;showin­fo=0&amp;controls=0&amp;HD=1;iv_load_policy=3");
-    iframe.style.width  = 400;
-    iframe.style.height = 300;
-	document.body.appendChild(iframe);
+
+function change_video(id){
+	console.log("here");
+
+	player = new YT.Player('player', {
+		width: 400,
+		height: 300,
+		videoId: id,
+		events: {
+			'onPlayerReady': onPlayerReady,
+			'onPlayerStateChange' : onPlayerStateChange
+		}
+	});
+}
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady(){
+	id = 'rVeMiVU77wo';
+
+	player = new YT.Player('player', {
+		width: 400,
+		height: 300,
+		videoId: id,
+		events: {
+			'onPlayerReady': onPlayerReady,
+			'onPlayerStateChange' : onPlayerStateChange
+		}
+	});
+}
+var done = false;
+function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+}
+function stopVideo() {
+        player.stopVideo();
 }
