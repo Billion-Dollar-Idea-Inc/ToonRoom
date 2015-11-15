7eba5441443c7bd8e55a05de6e8072e9249a2cc0
Ethan Schoen <e.schoen@yahoo.com>
2015-11-15 00:24:38 -0500

    STUFF IS WORKING OH MY GAWD, LAWDY LAWDY

--- static/chat.js
+++ static/chat.js
@@ -1,12 +1,25 @@
+var link;
 function revealBox(){
 	document.getElementById("plus_sign").style.visibility = "hidden";
 	document.getElementById("input").style.visibility = "visible";
 	document.getElementById("submit").style.visibility = "visible";
 }
 function back_to_plus(){
-	var Guess = document.getElementById('input').value;
-	alert(Guess);
+	link = document.getElementById('input').value;
+	//alert(link);
 	document.getElementById("plus_sign").style.visibility = "visible";
 	document.getElementById("input").style.visibility = "hidden";
 	document.getElementById("submit").style.visibility = "hidden";
+	update_video()
+}
+function update_video(){
+	var id = link.substring(32);
+	var iframe = document.createElement("iframe");
+    iframe.setAttribute("src",
+          "https://www.youtube.com/embed/" + id 
+        + "?rel=0&amp;autoplay=1&amp;showin­fo=0&amp;controls=0&amp;HD=1;iv_load_policy=3");
+    
+    iframe.style.width  = 400;
+    iframe.style.height = 300;
+	document.body.appendChild(iframe)
 }
