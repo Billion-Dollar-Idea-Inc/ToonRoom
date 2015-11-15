3ec2568edf9e41c9c73d8db01d3d410e2d2f578a
Derek Schuster <dschust1@binghamton.edu>
2015-11-14 19:08:22 -0500

    fixed merge problems

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import *
-<<<<<<< HEAD
 import random
-=======
->>>>>>> 09bb85a4b1acbc117bd5de96767f2b7064300d12
+
 
 app = Flask(__name__)
 
 @app.route('/')
-<<<<<<< HEAD
 def index():
     return render_template('index.html')
 
@@ -22,14 +19,13 @@ def join():
 def chat(room):
 	# Room validation code
 	return render_template('chat.html', room_num=room)
-=======
+
 def TuneRoomHome():
 	return render_template('index.html')
 
 @app.route('/chat')
 def TuneRoomChat():
 	return render_template('chat.html')
->>>>>>> 09bb85a4b1acbc117bd5de96767f2b7064300d12
 
 if __name__ == '__main__':
 	app.debug = True
