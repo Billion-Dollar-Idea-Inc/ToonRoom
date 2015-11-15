5783a859c698ed5301425b36ede3873f980cae7d
Ethan Schoen <e.schoen@yahoo.com>
2015-11-14 22:02:38 -0500

    nothing really changed

--- static/chat.js
+++ static/chat.js
@@ -2,13 +2,11 @@ var plus = document.getElementById("plus_sign");
 var input = document.getElementById("input");
 var submit = document.getElementById("submit");
 function revealBox(){
-	alert("hello");
 	document.getElementById("plus_sign").style.visibility = "hidden";
 	document.getElementById("input").style.visibility = "visible";
 	document.getElementById("submit").style.visibility = "visible";
 }
 function back_to_plus(){
-	alert("yo");
 	document.getElementById("plus_sign").style.visibility = "visible";
 	document.getElementById("input").style.visibility = "hidden";
 	document.getElementById("submit").style.visibility = "hidden";
