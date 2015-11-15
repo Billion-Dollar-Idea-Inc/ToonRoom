b06c9447b2ec5386cd2f3eab2cf63069856aef18
Derek Schuster <dschust1@binghamton.edu>
2015-11-14 19:46:45 -0500

    start of database

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from contextlib import closing
 from flask import *
 import random
+import sqlite3
 
+
+DATABASE = '/schema.db'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
 def index():
@@ -10,9 +15,17 @@ def index():
 
 @app.route('/join', methods=['POST'])
 def join():
-	room_num = random.randint(1, 1000000)
 	# Redirect to /chat/<room>
-	# DB find
+
+	# check database for open spot
+	if open:
+#room_num = the spot of the open one
+		pass
+	else:
+		while True:
+			room_num = random.randint(10000, 99999)
+#			if room_num has not been used:
+			break
 	return redirect(url_for('chat', room=room_num))
 
 @app.route('/chat/<room>', methods =['GET', 'POST'])
@@ -20,13 +33,17 @@ def chat(room):
 	# Room validation code
 	return render_template('chat.html', room_num=room)
 
-def TuneRoomHome():
-	return render_template('index.html')
+def init_db():
+		with closing(connect_db()) as db:
+				with app.open_resource('schema.sql', mode='r') as f:
+						db.cursor().executescript(f.read())
+				db.commit()
 
-@app.route('/chat')
-def TuneRoomChat():
-	return render_template('chat.html')
+def connect_db():
+		return sqlite3.connect(DATABASE)
+
 
 if __name__ == '__main__':
+	init_db()
 	app.debug = True
 	app.run(host='0.0.0.0')# makes server publicly available
